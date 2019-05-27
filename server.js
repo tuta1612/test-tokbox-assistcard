75490e91711ec4a9f77e3e573ac796c9dcb528fd
Desarrollo <desarrolloaci@gmail.com>
2019-05-27 10:32:53 -0300

    rta

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/waitingroom', function (req, res) {
 
 app.post('/Api/Turn/CheckStatus',function (req, res) {
     //var json = '{"ResponseEntity": {"QtyPatientsBefore": 2,"ElapsedPercentual": 33,"TakenByMedicalSpecialist": false,"SuggestedSecondsToRetry": 30},"ApplicationName": null,"StatusCode": 200,"ApplicationStatusCodeSource": null,"StatusCodeParameters": null,"ApplicationEventTableName": null,"ErrorId": 0,"Message": null}';    
-    var rta = {"req": req, "req.body": req.body};
-    var json = JSON.stringify(rta);
-    res.send(json);
+    //var rta = {"req": req, "req.body": req.body};
+    //var json = JSON.stringify(rta);
+    res.send("rta");
 });
